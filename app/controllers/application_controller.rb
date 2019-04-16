6380033fe63b88bddd8faddad3ddef8e3ec8dc96
Yevgeniy <yevgeniyerot@gmail.com>
2019-04-16 18:00:46 -0400

    removed old methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,6 @@ class ApplicationController < ActionController::Base
   before_action :get_instance, only: [:show, :edit, :update, :destroy]
   helper_method :current_cart, :current_user, :logged_in?
 
-  def current_user
-    return unless session[:user_id]
-    @current_user ||= User.find(session[:user_id])
-  end
-
-
   def current_cart
     if session[:cart_id]
       CartTool.find(session[:cart_id])
@@ -20,12 +14,6 @@ class ApplicationController < ActionController::Base
       CartTool.new
     end
   end
-
-
-  def logged_in?
-    current_user != nil
-  end
-
 
   def show; end
 
