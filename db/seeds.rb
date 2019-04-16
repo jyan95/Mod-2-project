61084698acda314e4e51b55e8736a394a3f46fad
github username <github email address>
2019-04-16 11:53:09 -0400

    seeded tools, users, and categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -51,19 +51,14 @@
 # - lawn tools
 # - leaf blower
 
-Category.create(name: "General Purpose")
-Category.create(name: "Air & Pneumatic")
-Category.create(name: "Power Cleaning")
-Category.create(name: "Testing")
-Category.create(name: "Landscaping")
-Category.create(name: "Power Tools")
+
 
 require 'faker'
 
 # Faker for User
 User.destroy_all
-Category.destroy_all
-Tool.destroy_all
+# Category.destroy_all
+# Tool.destroy_all
 UserTool.destroy_all
 Cart.destroy_all
 
@@ -83,6 +78,13 @@ end
 
 # Faker for Category
 
+# Category.create(name: "General Purpose")
+# Category.create(name: "Air & Pneumatic")
+# Category.create(name: "Power Cleaning")
+# Category.create(name: "Testing")
+# Category.create(name: "Landscaping")
+# Category.create(name: "Power Tools")
+
 # 20.times do
 #   Category.create(name: Faker::Commerce.department(1))
 # end
@@ -94,7 +96,56 @@ end
 # end
 
 # Faker for Tool
-#
+
+Tool.create(name:'Bender',category_id: 13)
+Tool.create(name:'Clamp',category_id: 13)
+Tool.create(name:'Crimper',category_id: 13)
+Tool.create(name:'Cutter',category_id: 13)
+Tool.create(name:'Plastering',category_id: 13)
+Tool.create(name:'Hand Saw',category_id: 13)
+Tool.create(name:'Toolkits',category_id: 13)
+Tool.create(name:'Masonry',category_id: 13)
+Tool.create(name:'Vice',category_id: 13)
+Tool.create(name:'Chisel',category_id: 13)
+Tool.create(name:'Lever',category_id: 13)
+
+Tool.create(name:'Air Compressor',category_id: 14)
+Tool.create(name:'Air Hose',category_id: 14)
+Tool.create(name:'Pneumatic Hose',category_id: 14)
+Tool.create(name:'Hose Clamp',category_id: 14)
+
+Tool.create(name:'Power Drill',category_id: 15)
+Tool.create(name:'Heathen',category_id: 15)
+Tool.create(name:'Power Saw',category_id: 15)
+Tool.create(name:'Power Wrench',category_id: 15)
+Tool.create(name:'Pipe Cutter',category_id: 15)
+Tool.create(name:'Heat Gun',category_id: 15)
+Tool.create(name:'Cable Feeder',category_id: 15)
+Tool.create(name:'Power Crimper',category_id: 15)
+
+Tool.create(name:'Electrical Power Tester',category_id: 16)
+Tool.create(name:'Fiber Optics Tester',category_id: 16)
+Tool.create(name:'Temperature Tester',category_id: 16)
+Tool.create(name:'Humidity Tester',category_id: 16)
+Tool.create(name:'Phone Line Tester',category_id: 16)
+Tool.create(name:'Borescope',category_id: 16)
+Tool.create(name:'Light Meter',category_id: 16)
+Tool.create(name:'Sound Level Meter',category_id: 16)
+
+Tool.create(name:'Mower',category_id: 17)
+Tool.create(name:'Trimmer',category_id: 17)
+Tool.create(name:'Hose',category_id: 17)
+Tool.create(name:'Shovel',category_id: 17)
+Tool.create(name:'Wheelbarrow',category_id: 17)
+Tool.create(name:'Pickaxe',category_id: 17)
+Tool.create(name:'Snowblower',category_id: 17)
+
+Tool.create(name:'Vacuum',category_id: 18)
+Tool.create(name:'Pressure Washer',category_id: 18)
+Tool.create(name:'Rake',category_id: 18)
+Tool.create(name:'Leaf Blower',category_id: 18)
+
+
 # 20.times do
 #   # byebug
 #   Tool.create(name: Faker::Commerce.product_name, category_id: Faker::Number.number(1))
